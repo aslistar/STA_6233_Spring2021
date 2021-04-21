6cf01b45563dc6a995a2282c27aa395940064226
Matthew Martinez <mjmpoetry@gmail.com>
2021-04-21 11:08:59 -0500

    Updating In-Class Code

--- 10_SQL/SQL.R
+++ 10_SQL/SQL.R
@@ -10,35 +10,61 @@ library(RSQLite)
   
 #Show Variables in Database file
   dbListFields(con, "Income")
+  
+#Basic SQL Structure
+  #SELECT statement
+    #What do you want
+    #On Variables
+  #FROM statement
+    #Where do you want the information from
+  #WHERE
+    #Any condition
+    #On Values in columns
+  #INNER JOIN
+    #How do you want these datatables to connect
+    #On Columns
+  #ORDER BY
+    #How do you want the resulting query to look
+    #On Values in columns
 
 #Show Unique Values in a Variable in Database file
-  dbGetQuery(con, "
+  org<-dbGetQuery(con, "
             SELECT DISTINCT
             Orgs
             
             FROM Income")
   
-  dbGetQuery(con, "
+  reg<-dbGetQuery(con, "
             SELECT DISTINCT
             Region
             
             FROM Income")
   
-  dbGetQuery(con, "
+  both<-dbGetQuery(con, "
             SELECT DISTINCT
             Orgs,
             Region
             
             FROM Income")
   
+  both_order <- dbGetQuery(con, "
+            SELECT DISTINCT
+            Orgs,
+            Region
+            
+            FROM Income
+             
+            ORDER BY Region")
+
+  
 #### Do a Simple Query ####
   #If we want all the data
-  all<-dbGetQuery(con, "
+  income<-dbGetQuery(con, "
             SELECT * 
             FROM Income")
   
   #If we want to limit our data
-  dbGetQuery(con, "
+  limit<-dbGetQuery(con, "
             SELECT * 
             FROM Income
             Limit 100;")
@@ -62,23 +88,39 @@ library(RSQLite)
               
             FROM Income
               
-            WHERE Orgs='Texas Best'
-              
-            Limit 100;")
+            WHERE Orgs='Texas Best'")
   
 #### Complete a Table Join ####
   # See what we have in our Expenses table?
   # What should we join on?
   # Bring in Expenses column from Expenses table
+  dbListFields(con, "Expenses")
+  
+  exp1<-dbGetQuery(con, "
+            SELECT *
+            FROM Expenses
+            Limit 100;")
+  exp1<-dbGetQuery(con, "
+            SELECT Expenses
+            FROM Expenses")
+  
+  #Find Unique combinations of Orgs, Months, Regions, Years
+  comb<-dbGetQuery(con, "
+                   SELECT DISTINCT
+                   Orgs,
+                   Months,
+                   Region,
+                   Years
+                   FROM expenses")
+  Exp<-dbGetQuery(con, "
+                  SELECT *
+                  FROM expenses")
   
   #Inner Join
   join<- dbGetQuery(con, "
             SELECT
-            i.orgs,
-            i.Months,
-            i.Income,
-            e.Expenses,
-            e.U_ID
+            i.*,
+            e.Expenses as Money_Out
             
             FROM 
             Income i
@@ -101,25 +143,51 @@ library(RSQLite)
                 ON i.U_ID = e.U_ID")
   #61291
   
+  #Merge in R
+  merge(x, y, all.x=T, all.y=T) #Inner
+  merge(x, y, all.x=T, all.y=F) #Left
+  merge(x, y, all.x=F, all.y=T) #Right
+  merge(x, y, all.x=F, all.y=F) #Outer
+  
 #### Write Tables to Database ####
-  dbGetQuery(con, "CREATE TABLE some AS
-             SELECT 
-             Orgs,
-             Months,
-             Year_ID
-             
-             FROM Income
-             
-             WHERE Orgs='Texas Best'")
+  dbGetQuery(con, "CREATE TABLE join2 AS
+            SELECT
+            i.orgs,
+            i.Months,
+            i.Income,
+            e.Expenses as Money_Out,
+            e.U_ID as Expense_ID
+            
+            FROM 
+            Expenses e
+            
+            LEFT JOIN Income i
+                ON i.U_ID = e.U_ID")
   
   #See if it wrote correctly
   dbListTables(con)
-  dbGetQuery(con, "
+  test<-dbGetQuery(con, "
             SELECT *
-            FROM some")
+            FROM join2")
   
   #Delete It
-  dbGetQuery(con, "DROP TABLE some")
+  dbGetQuery(con, "DROP TABLE join2")
   dbListTables(con)
   
 #Interactions with R
+  Orgs_e<-c("Tri West", "Tri South", "Texas Best", "Helping Here", "Silver and Black Give Back",
+                   "Mikes Tots", "Purple Cross")
+  #If we want to only bring back certain values
+  for(i in 1:5){
+   eval(parse(text=paste0("some_",i, "<-dbGetQuery(con, paste0(\"
+            SELECT 
+            Orgs,
+            Months,
+            Year_ID
+              
+            FROM Income
+              
+            WHERE Orgs='", Orgs_e[i], "'\"))")))
+  }
+  
+rm(some)  
